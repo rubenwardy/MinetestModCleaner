fc8975ba1247e29203b6f5863081297a0cda3218
rubenwardy <anjayward@gmail.com>
2014-02-05 13:44:08 +0000

    Create example.py

---
+++ example.py
@@ -0,0 +1,17 @@
+from modmanager import *
+
+# Create cleaner
+cleaner = ModManager("working_directory")
+
+# Run on existing mod
+# Parameters
+# location: path to the archive
+# title: the human readable name of the mod
+# name: the mod namespace
+# (optional) desc: the description to go in description.txt
+# (optional) image: the path to the mod image to be copied to screenshot.png
+res = cleaner.run("food.zip","Food Mod","food","This is the food mod","")
+# return path to new archive
+
+# The same, but gets from github instead of location
+res = mm.githubGet("https://github.com/rubenwardy/awards","Achievements","awards","This is the award mod","")
